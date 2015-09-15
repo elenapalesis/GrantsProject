09602d13e759a774d252aabeeba74b393804bea3
elenapalesis <elenapalesis@gmail.com>
2015-09-15 19:33:12 -0400

    removed unnecessary files and renamed the remaining so that they made more sense

--- Processed Data Files - Excel/sql code/replace_nonames.sql
+++
@@ -1,7 +0,0 @@
- UPDATE practice_table
- SET InvestigatorAllLastName = replace(InvestigatorAllLastName, '~', 'NA~')
- WHERE InvestigatorAllLastName LIKE '~%';
- 
- UPDATE practice_table
- SET InvestigatorAllLastName = replace(InvestigatorAllLastName, '~~', '~NA~')
- WHERE InvestigatorAllLastName LIKE '%~~%';

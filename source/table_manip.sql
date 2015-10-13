6a8d8cdce165c6ce871afb5ddd8322a25ef71c59
elenapalesis <elenapalesis@gmail.com>
2015-10-12 21:18:20 -0400

    updated documentation for mysql queries

--- source/table_manip.sql
+++ source/table_manip.sql
@@ -35,15 +35,20 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
 
+
+# remove column line from csv import
 DELETE FROM MAIN
 WHERE PIEmployeeID = "PI Employee ID";
 
-
+# add GrantYear column to streamline future yearly calculations
 ALTER TABLE Main ADD GrantYear int(4);
 UPDATE Main SET GrantYear = Year(SubmittedDate);
 
+
+# FlagBad column flags rows where investigator names are empty or placeholders.
 ALTER TABLE Main ADD FlagBad int(4);
 
+# this flags a long list of investigator names that are only placeholders
 UPDATE Main SET FlagBad = 1
 WHERE 	(InvestigatorALLLastName LIKE '%MATR %') OR
 (InvestigatorALLLastName LIKE '%To Be hired%') OR
@@ -52,6 +57,7 @@ WHERE 	(InvestigatorALLLastName LIKE '%MATR %') OR
 (InvestigatorALLLastName LIKE '%TBH%') OR
 (InvestigatorALLLastName LIKE '%TBD%') OR
 (InvestigatorALLLastName LIKE '%TBA%') OR
+(InvestigatorALLLastName LIKE '%TBN%') OR
 (InvestigatorALLLastName LIKE '%Technician%') OR
 (InvestigatorALLLastName LIKE '%Lab%') OR
 (InvestigatorALLLastName LIKE '%Specialist%') OR
@@ -78,6 +84,7 @@ WHERE 	(InvestigatorALLLastName LIKE '%MATR %') OR
 (InvestigatorALLFirstName LIKE '%TBH%') OR
 (InvestigatorALLFirstName LIKE '%TBD%') OR
 (InvestigatorALLFirstName LIKE '%TBA%') OR
+(InvestigatorALLFirstName LIKE '%TBN%') OR
 (InvestigatorALLFirstName LIKE '%Technician%') OR
 (InvestigatorALLFirstName LIKE '%Lab%') OR
 (InvestigatorALLFirstName LIKE '%Specialist%') OR
@@ -98,16 +105,86 @@ WHERE 	(InvestigatorALLLastName LIKE '%MATR %') OR
 (InvestigatorALLFirstName LIKE '%~~%') OR
 (InvestigatorALLFirstName LIKE '~%');
 
-SELECT Count(FlagBad) FROM Main;
+
+# How many grants submitted have missing members or placeholder names?
+SELECT COUNT(FlagBad)FROM Main;
 
 
 
+########################### Investigators --------------
+DROP TABLE Investigators;
 
+CREATE TABLE Investigators
+SELECT
+	InvestigatorID, 
+	InvestigatorLastName,
+	InvestigatorFirstName,
+	InvestigatorDept,
+	COUNT(InvestigatorID) as NumberProposals,
+	SUM(Awarded) as NumberAwarded
+FROM TwoMode
+GROUP BY InvestigatorID;
+
+ALTER TABLE Investigators
+
+
+
+########################### Grants -------------------------
+
+DROP TABLE GrantTable;
+
+# create GrantTable to calculate initial information about each grant proposal
+CREATE TABLE GrantTable
+SELECT
+	InstitutionNumber,
+	SubmittedDate,
+	YEAR(SubmittedDate) as GrantYear,
+	Awarded,
+	COUNT(InvestigatorID) as NumberInvestigators
+FROM TwoMode
+GROUP BY InstitutionNumber;
+
+
+# SumTeamSuccessful is the number of grants funded per investigator
+# summed over all investigators participating in the project
+ALTER TABLE GrantTable
+ADD SumTeamSuccessful int(11);
+
+
+# SumTeamProposed is the number of grant projects each investigator
+# has participated in, summed over all investigators of the grant
+ALTER TABLE GrantTable
+ADD SumTeamProposed int(11);
+
+
+# calulating SumTeamSuccessful and SumTeamProposed columns
+UPDATE GrantTable as GT
+INNER JOIN (
+	SELECT 
+		TM.InstitutionNumber,
+		SUM(I.NumberProposals) as SumTeamProposed,
+		SUM(I.NumberAwarded) as SumTeamSuccessful
+	FROM TwoMode as TM
+	INNER JOIN Investigators as I on TM.InvestigatorID = I.InvestigatorID
+	GROUP BY TM.InstitutionNumber) as N on GT.InstitutionNumber = N.InstitutionNumber
+SET GT.SumTeamProposed = N.SumTeamProposed,
+	GT.SumTeamSuccessful = N.SumTeamSuccessful;
+
+
+# TeamSuccessRate is the ratio of Successful/Proposed columns:
+ALTER TABLE GrantTable
+ADD TeamSuccessRate FLOAT;
+
+UPDATE GrantTable
+SET TeamSuccessRate = SumTeamSuccessful/SumTeamProposed;
 
 
 
 ########################### TwoMode -------------------------
 
+# This TwoMode table pairs Grants to Investigators. To generate it,
+# we run the source/build_TwoMode.R file, and then import the resultant
+# csv file into the database.
 
 CREATE TABLE TwoMode
 (
@@ -136,7 +213,7 @@ NIHGrant int,
 AwardedNIH int
 );
 
-DELETE FROM TwoMode;
+#DELETE FROM TwoMode;
 
 LOAD DATA LOCAL INFILE '/Users/elenapalesis/dropbox/grant collaborations 2015/data files/TwoMode_table.txt' 
 INTO TABLE TwoMode 
@@ -144,14 +221,75 @@ FIELDS TERMINATED BY '\t'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
+# delete the column headings generated when importing the csv file
 DELETE FROM TwoMode
 WHERE PIEmployeeID = "PI Employee ID";
+
+# Create a unique ID for each investigator by concatenating First,Last names
+ALTER TABLE TwoMode
+ADD InvestigatorID varchar(255) FIRST;
+
+UPDATE TwoMode
+SET InvestigatorID = CONCAT(InvestigatorLastName,',',InvestigatorFirstName);
+
+
+# Bring InstitutionNumber to the front of the table
+ALTER TABLE TwoMode
+MODIFY InstitutionNumber varchar(255) FIRST;
+
+# Create GrantYear column to streamline future calculations
+ALTER TABLE TwoMode
+ADD GrantYear int(4);
+
+UPDATE TwoMode
+SET GrantYear = YEAR(SubmittedDate);
+
+
+#Delete rows where investigator is a nameholder/blank
+DELETE FROM  TwoMode
+WHERE 	(InvestigatorID LIKE '%MATR %') OR
+(InvestigatorID LIKE '%To Be hired%') OR
+(InvestigatorID LIKE '%Postdoc%') OR
+(InvestigatorID LIKE '%Research%') OR
+(InvestigatorID LIKE '%TBH%') OR
+(InvestigatorID LIKE '%TBD%') OR
+(InvestigatorID LIKE '%TBA%') OR
+(InvestigatorID LIKE '%TBN%') OR
+(InvestigatorID LIKE '%Technician%') OR
+(InvestigatorID LIKE '%Lab%') OR
+(InvestigatorID LIKE '%Specialist%') OR
+(InvestigatorID LIKE '%Coordinator%') OR
+(InvestigatorID LIKE '%To be determined%') OR
+(InvestigatorID LIKE '%To be named%') OR
+(InvestigatorID LIKE '%determined%') OR
+(InvestigatorID LIKE '%To be hired%') OR
+(InvestigatorID LIKE '%Project%') OR
+(InvestigatorID LIKE '%Intern%') OR
+(InvestigatorID LIKE '%Assistant%') OR
+(InvestigatorID LIKE '%Counselor%') OR
+(InvestigatorID LIKE '%PhD%') OR
+(InvestigatorID LIKE '%student%') OR
+(InvestigatorID LIKE '%worker%') OR
+(InvestigatorID LIKE '%Program%') OR
+(InvestigatorID LIKE '%Coordinator%') OR
+(InvestigatorID LIKE '%,') OR
+(InvestigatorID LIKE ',%') OR
+(InvestigatorID LIKE ',');
+
+
+
+
+
+
 
 
 
 ########################### PairedInvestigators --------------
 DROP TABLE PairedInvestigators;
 
+
+# Create a table that pairs Investigators if they have worked
+# together on a grant 
 
 CREATE TABLE PairedInvestigators
 (
@@ -234,7 +372,12 @@ DROP Investigator2FirstName;
 
 
 
+#Add Year column
+ALTER TABLE PairedInvestigators
+ADD GrantYear int(4) AFTER Investigator2Dept;
 
+UPDATE PairedInvestigators
+SET GrantYear = Year(SubmittedDate);
 
 
 
@@ -247,6 +390,7 @@ WHERE 	(InvestigatorID1 LIKE '%MATR %') OR
 (InvestigatorID1 LIKE '%TBH%') OR
 (InvestigatorID1 LIKE '%TBD%') OR
 (InvestigatorID1 LIKE '%TBA%') OR
+(InvestigatorID1 LIKE '%TBN%') OR
 (InvestigatorID1 LIKE '%Technician%') OR
 (InvestigatorID1 LIKE '%Lab%') OR
 (InvestigatorID1 LIKE '%Specialist%') OR
@@ -274,6 +418,7 @@ WHERE 	(InvestigatorID1 LIKE '%MATR %') OR
 (InvestigatorID2 LIKE '%TBH%') OR
 (InvestigatorID2 LIKE '%TBD%') OR
 (InvestigatorID2 LIKE '%TBA%') OR
+(InvestigatorID2 LIKE '%TBN%') OR
 (InvestigatorID2 LIKE '%Technician%') OR
 (InvestigatorID2 LIKE '%Lab%') OR
 (InvestigatorID2 LIKE '%Specialist%') OR
@@ -311,7 +456,6 @@ MODIFY Investigator2Dept varchar(255) AFTER Investigator1Dept;
 
 
 ##### Yearly Statistics --------------------
-
 
 
 CREATE TABLE YearlyStatistics
@@ -361,5 +505,110 @@ SET RatioSingle = SingleInvestigators/NumberProposals,
 
 
 
+#--------
+SELECT InvestigatorID1, InvestigatorID2
+FROM PairedInvestigators
+WHERE InvestigatorID1 != InvestigatorID2;
 
 
+UPDATE
+	YearlyStatistics as S
+	INNER JOIN (
+		SELECT YEAR(SubmittedDate) AS GrantYear,
+		COUNT(InstitutionNumber) AS SingleInvestigators
+		FROM Main
+		WHERE TeamSize = 1
+		GROUP BY YEAR(SubmittedDate)
+	) as N on S.GrantYear = N.GrantYear
+SET S.SingleInvestigators = N.SingleInvestigators;
+
+##### Paired Grants --------------------
+
+CREATE TABLE temp1_PairedGrants
+SELECT 
+	TM1.InstitutionNumber as InstitutionNumber1, 
+	TM2.InstitutionNumber as InstitutionNumber2,
+	TM1.Awarded as Awarded1,
+	TM2.Awarded as Awarded2,
+	YEAR(TM1.SubmittedDate) as GrantYear1,
+	YEAR(TM2.SubmittedDate) as GrantYear2 
+FROM TwoMode as TM1
+INNER JOIN 
+	TwoMode as TM2
+	on TM1.InvestigatorID = TM2.InvestigatorID;
+	
+
+UPDATE temp1_PairedGrants
+SET InstitutionNumber1 = InstitutionNumber2,
+	InstitutionNumber2 = InstitutionNumber1
+WHERE InstitutionNumber1 < InstitutionNumber2;
+
+CREATE TABLE PairedGrants
+SELECT DISTINCT *
+FROM temp1_PairedGrants;
+
+DROP TABLE temp1_PairedGrants;
+
+##### Paired Departments --------------------
+DROP TABLE PairedDepartments;
+CREATE TABLE temp1_PairedDepartments
+SELECT
+	TM1.InvestDept1 as Dept1,
+	TM2.InvestDept2 as Dept2,
+	TM1.Awarded1 as Awarded1,
+	TM2.Awarded2 as Awarded2,
+	TM1.GrantYear1 as GrantYear1,
+	TM2.GrantYear2 as GrantYear2
+FROM (
+	SELECT 
+		InvestigatorDept as InvestDept1,
+		InstitutionNumber as InstitNum1,
+		InvestigatorID as InvestID1,
+		Awarded as Awarded1,
+		YEAR(SubmittedDate) as GrantYear1
+	FROM TwoMode
+	) as TM1
+INNER JOIN
+	(
+	SELECT
+		InvestigatorDept as InvestDept2,
+		InstitutionNumber as InstitNum2,
+		InvestigatorID as InvestID2,
+		Awarded as Awarded2,
+		YEAR(SubmittedDate) as GrantYear2
+	FROM TwoMode
+	) as TM2
+on TM1.InstitNum1 = TM2.InstitNum2;
+
+SELECT
+	Dept1,
+	Dept2,
+	SUM(Awarded1) as SumAwarded,
+	COUNT(*) as Weight
+FROM PairedDepartments
+GROUP BY Dept1, Dept2;
+
+
+
+SELECT * FROM
+(
+	SELECT 
+		InvestigatorDept as InvestDept1,
+		InstitutionNumber as InstitNum1,
+		InvestigatorID as InvestID1,
+		Awarded as Awarded1,
+		YEAR(SubmittedDate) as GrantYear1
+	FROM TwoMode
+	) as TM1
+INNER JOIN
+	(
+	SELECT
+		InvestigatorDept as InvestDept2,
+		InstitutionNumber as InstitNum2,
+		InvestigatorID as InvestID2,
+		Awarded as Awarded2,
+		YEAR(SubmittedDate) as GrantYear2
+	FROM TwoMode
+	) as TM2
+on TM1.InstitNum1 = TM2.InstitNum2
+WHERE TM1.InvestID1 != TM2.InvestID2 AND TM1.InstitNum1 != TM2.InstitNum2;
